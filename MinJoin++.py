b60076d04381aedf946be48a1dc2946484dc3225
lborovina1 <148221082+lborovina1@users.noreply.github.com>
2024-08-14 19:53:27 +0200

    MinJoin++ sa SeaHash

--- MinJoin++.py
+++ MinJoin++.py
@@ -1,9 +1,7 @@
-from python import math
 from itertools import groupby, combinations
 from collections import Counter, defaultdict
 from python import Levenshtein as lev
 from python import mmh3
-from python import seahash
 from python import Bio.SeqIO
 import re
 import time
@@ -11,16 +9,100 @@ import time
 def Pi(x):
     return mmh3.hash(x)
 
-def f(x):
-    """if isinstance(x, str):
-        x = bytes(x, 'utf-8')
-    return seahash.hash(x)"""
-    return mmh3.hash(x) 
+def sea_swap64(x):
+    return (((x << 56u64) & 0xff00000000000000u64) | 
+            ((x << 40u64) & 0x00ff000000000000u64) | 
+            ((x << 24u64) & 0x0000ff0000000000u64) |
+            ((x << 8u64)  & 0x000000ff00000000u64) |
+            ((x >> 8u64)  & 0x00000000ff000000u64) |
+            ((x >> 24u64) & 0x0000000000ff0000u64) |
+            ((x >> 40u64) & 0x000000000000ff00u64) |
+            ((x >> 56u64) & 0x00000000000000ffu64))
+
+def diffuse(val):
+    val *= 0x6eed0e9da4d94a4fu64
+    a = val >> 32u64
+    b = val >> 60u64
+    val ^= a >> b
+    val *= 0x6eed0e9da4d94a4fu64
+    return val
+
+def f(key, seed = 0):
+    a = 0x16f11fe89b0d677cu64 ^ u64(seed)
+    b = 0xb480a793d8e6c86cu64
+    c = 0x6fe2e5aaf078ebc9u64
+    d = 0x14f994a4c5259381u64
+    pad = [u64(0)]
+
+    p = Ptr[u64](key.ptr)
+    i, l = 0, len(key)
+    while l >= 32:
+        a ^= p[i]
+        b ^= p[i + 1]
+        c ^= p[i + 2]
+        d ^= p[i + 3]
+        a = diffuse(a)
+        b = diffuse(b)
+        c = diffuse(c)
+        d = diffuse(d)
+        i += 4
+        l -= 32
+    if 25 <= l <= 31:
+        a ^= p[i]
+        b ^= p[i + 1]
+        c ^= p[i + 2]
+        str.memcpy(pad.arr.ptr.as_byte(), (p + 3).as_byte(), l - 24)
+        d ^= pad[0]
+        a = diffuse(a)
+        b = diffuse(b)
+        c = diffuse(c)
+        d = diffuse(d)
+    elif l == 24:
+        a ^= p[i]
+        b ^= p[i + 1]
+        c ^= p[i + 2]
+        a = diffuse(a)
+        b = diffuse(b)
+        c = diffuse(c)
+    elif 17 <= l <= 23:
+        a ^= p[i]
+        b ^= p[i + 1]
+        str.memcpy(pad.arr.ptr.as_byte(), (p + 2).as_byte(), l - 16)
+        c ^= pad[0]
+        a = diffuse(a)
+        b = diffuse(b)
+        c = diffuse(c)
+    elif l == 16:
+        a ^= p[i]
+        b ^= p[i + 1]
+        a = diffuse(a)
+        b = diffuse(b)
+    elif 9 <= l <= 15:
+        a ^= p[i]
+        str.memcpy(pad.arr.ptr.as_byte(), (p + 1).as_byte(), l - 8)
+        b ^= pad[0]
+        a = diffuse(a)
+        b = diffuse(b)
+    elif l == 8:
+        a ^= p[i]
+        a = diffuse(a)
+    elif 1 <= l <= 7:
+        str.memcpy(pad.arr.ptr.as_byte(), p.as_byte(), l)
+        a ^= pad[0]
+        a = diffuse(a)
+        a ^= b
+        c ^= d
+        a ^= c
+        a ^= u64(len(key))
+    return sea_swap64(diffuse(a))
+
+def floor(x):
+    return int(x) if x >= 0 else int(x) - 1 if x != int(x) else int(x)
 
 def find_anchors(s, T, Pi):
     A = [0]
     q = 3
-    r = math.floor((len(s) - q + 1 - T) / (2 * T + 2))
+    r = floor((len(s) - q + 1 - T) / (2 * T + 2))
     
     h = [Pi(s[i:i + q]) for i in range(len(s) - q + 1)]
 
@@ -78,7 +160,7 @@ def split_into_windows(sequence, window_size=1000, step_size=500):
         windows.append(sequence[start:end])
     return windows
 
-def compare_windows(seq1, seq2, windows1, windows2, K, T):
+def compare_windows(windows1, windows2, K, T):
     results = []
     for win1 in windows1:
         for win2 in windows2:
@@ -94,7 +176,7 @@ def process_pair(pair, K, T):
         if chrom1 == chrom2:
             windows1 = split_into_windows(sequence1)
             windows2 = split_into_windows(sequence2)
-            comparisons = compare_windows(sequence1, sequence2, windows1, windows2, K, T)
+            comparisons = compare_windows(windows1, windows2, K, T)
             return comparisons
         
         return None
@@ -133,12 +215,14 @@ if __name__ == "__main__":
     T = 20 + K / 8.
     
     comparison_results = compare_adjacent_sequences(sequences, K, T)
+    #print(len(sequences))
 
     cnt = 0
     for result in comparison_results:
         #chrom1, start1, end1 = result['sequence1']
         #chrom2, start2, end2 = result['sequence2']
         print(len(result))
-    
+
     end_time = time.time()
     print(end_time - start_time)
+
